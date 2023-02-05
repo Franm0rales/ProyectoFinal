4112651a74b253fe0983bf3cc6c550efffb6dae6
Franm0rales <PICASSOMORALES@GMAIL.COM>
2023-02-05 15:56:57 +0100

    af

--- Front_Proyecto_Final/vite-project/src/components/Layout/Layout.jsx
+++ Front_Proyecto_Final/vite-project/src/components/Layout/Layout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import "../../../src/index.css";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 import "../Navbar/Navbar.css";
