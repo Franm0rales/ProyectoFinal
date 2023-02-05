f3ec02c9da95c888b8ca2e58a8bcb6c02273af4e
Franm0rales <PICASSOMORALES@GMAIL.COM>
2023-02-05 14:05:38 +0100

    front

--- Front_Proyecto_Final/vite-project/src/components/CardAdmin/CardAdmin.jsx
+++ Front_Proyecto_Final/vite-project/src/components/CardAdmin/CardAdmin.jsx
@@ -1,45 +1,24 @@
 import "./CardAdmin.css"
-import galeria1 from "../../assets/galeria1.jpg"
 
-export default function CardAdmin() {
+export default function CardAdmin(user) {
   return (
-    <div class="container d-flex align-items-center justify-content-center position-relative flex-wrap pt-4">
-    <div class="card d-flex position-relative flex-column">
-        <div class='imgContainer'>
-            <img src='https://images.pexels.com/photos/1456268/pexels-photo-1456268.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'/>
+
+    <div className="container pt-4 pb-3">
+      <div className="card   ">
+        <div className="imgContainer">
+          <img src="https://images.pexels.com/photos/1456268/pexels-photo-1456268.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
         </div>
-        <div class="content">
-            <h2>Card One</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget velit tristique, sollicitudin leo viverra, suscipit neque. Aliquam ut facilisis urna, in pretium nibh.  Morbi in leo in eros commodo volutpat ac sed dolor.</p>
-            <a href="#" class="btn btn-primary">
-          Editar Alumno
-        </a>
+        <div className="content">
+          <h2>{user.nombre} {user.apellidos}</h2>
+          <p>{user.email}</p>
+          <p>{user.ciudad}</p>
+          <p>{user.telefono}</p>
+          
         </div>
+        <a href="#" className="btn btn-primary">
+            Editar Alumno
+          </a>
+      </div>
     </div>
-    <div class="card d-flex position-relative flex-column">
-        <div class='imgContainer'>
-            <img src='https://images.pexels.com/photos/196667/pexels-photo-196667.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'/>
-        </div>
-        <div class="content">
-            <h2>Card Two</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget velit tristique, sollicitudin leo viverra, suscipit neque. Aliquam ut facilisis urna, in pretium nibh.  Morbi in leo in eros commodo volutpat ac sed dolor.</p>
-        <a href="#" class="btn btn-primary">
-          Editar Alumno
-        </a>
-        </div>
-    </div>
-    <div class="card d-flex position-relative flex-column">
-        <div class='imgContainer'>
-            <img src='https://images.pexels.com/photos/847483/pexels-photo-847483.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'/>
-        </div>
-        <div class="content">
-            <h2>Card Three</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget velit tristique, sollicitudin leo viverra, suscipit neque. Aliquam ut facilisis urna, in pretium nibh.  Morbi in leo in eros commodo volutpat ac sed dolor.</p>
-        <button href="#" class="btn btn-primary">
-          Editar Alumno
-        </button>
-        </div>
-    </div>
-</div>
   );
 }
--- Front_Proyecto_Final/vite-project/src/views/AlumnosAdmin/AlumnosAdmin.jsx
+++ Front_Proyecto_Final/vite-project/src/views/AlumnosAdmin/AlumnosAdmin.jsx
@@ -1,28 +1,25 @@
 import CardAdmin from "../../components/CardAdmin/CardAdmin";
+import { useState,useEffect } from "react";
+
 
 export default function AlumnosAdmin() {
-  return (
-    <div className="container-fluid">
+  const [users, setUsers] = useState(null);
+  useEffect(()=>{
+   const fetchData= async ()=> {
+      const response = await fetch("http://localhost:3000/user/allUsers");
+      const json = await response.json();
+      setUsers(json);
+    }
+    fetchData()
+  },[])
 
+  return (
+    <div className="">
     <div className=" d-flex px-3 flex-wrap d-grip gap-5">
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
-      <CardAdmin />
+    {users ? (
+        users.map((user) => <CardAdmin key={user.id} nombre={user.nombre} apellidos={user.apellidos} ciudad={user.ciudad} email={user.email} telefono={user.telefono}/>)
+      ) : (
+        <h4>No hay usuarios cargados...</h4>)}
     </div>
     </div>
   );
