eb5165df8aba60669ac3302115b9c877fe4b9d02
AlexGomezR <17gomezreyes@gmail.com>
2023-02-10 12:26:16 +0100

    cambiar avatar

--- Front_Proyecto_Final/vite-project/src/components/CardEditarUsuario/CardEditarUsuario.jsx
+++ Front_Proyecto_Final/vite-project/src/components/CardEditarUsuario/CardEditarUsuario.jsx
@@ -7,7 +7,13 @@ export default function CardEditarUsuario({ user, setUser }) {
   const [inputTelefono, setInputTelefono] = useState(false);
   const [inputCiudad, setInputCiudad] = useState(false);
   const [inputEmail, setInputEmail] = useState(false);
-  const [inputFecha, setInputFecha] = useState(false);
+  const [inputAvatar, setInputAvatar] = useState(false);
+  const [avatar2, setAvatar2] = useState("");
+  const [avatar3, setAvatar3] = useState("");
+  const [avatar4, setAvatar4] = useState("");
+  const [avatar5, setAvatar5] = useState("");
+  const [avatar7, setAvatar7] = useState("");
+  const [avatar8, setAvatar8] = useState("");
 
   const { authorization } = useAuthContext();
 
@@ -15,6 +21,62 @@ export default function CardEditarUsuario({ user, setUser }) {
   function handleInput(e) {
     let cambio = { ...editarUsuario, [e.target.name]: e.target.value };
     setEditarUsuario(cambio);
+  }
+
+  function matchAvatar(valor) {
+    setEditarUsuario({ avatar: valor });
+    switch (valor) {
+      case 2:
+        setAvatar2("border-primary");
+        setAvatar3("");
+        setAvatar4("");
+        setAvatar5("");
+        setAvatar7("");
+        setAvatar8("");
+
+        break;
+      case 3:
+        setAvatar2("");
+        setAvatar3("border-primary");
+        setAvatar4("");
+        setAvatar5("");
+        setAvatar7("");
+        setAvatar8("");
+
+        break;
+      case 4:
+        setAvatar2("");
+        setAvatar3("");
+        setAvatar4("border-primary");
+        setAvatar5("");
+        setAvatar7("");
+        setAvatar8("");
+        break;
+      case 5:
+        setAvatar2("");
+        setAvatar3("");
+        setAvatar4("");
+        setAvatar5("border-primary");
+        setAvatar7("");
+        setAvatar8("");
+        break;
+      case 7:
+        setAvatar2("");
+        setAvatar3("");
+        setAvatar4("");
+        setAvatar5("");
+        setAvatar7("border-primary");
+        setAvatar8("");
+        break;
+      case 8:
+        setAvatar2("");
+        setAvatar3("");
+        setAvatar4("");
+        setAvatar5("");
+        setAvatar7("");
+        setAvatar8("border-primary");
+        break;
+    }
   }
 
   async function onSubmit() {
@@ -32,9 +94,14 @@ export default function CardEditarUsuario({ user, setUser }) {
     if (response.status === 400) {
       alert("Error al recibir el body");
     } else if (response.status === 200) {
-      setInputNombre(!inputNombre);
+      setInputNombre(false);
+      setInputApellido(false);
+      setInputTelefono(false);
+      setInputCiudad(false);
+      setInputEmail(false);
+      setInputAvatar(false);
       setUser(await response.json());
-      alert(`usuario ${authorization.id} modificado correctamente`);
+      setEditarUsuario("");
     } else if (response.status === 409) {
       alert("usuario ya modificado");
     }
@@ -42,7 +109,17 @@ export default function CardEditarUsuario({ user, setUser }) {
   return (
     <section className="section about-section gray-bg" id="about">
       <div className="container">
-        <div className="row align-items-center flex-row-reverse">
+        <div className="row align-items-start justify-content-center">
+          <div className="col-lg-6 d-flex justify-content-center">
+            <div className="about-avatar">
+              <img
+                className="rounded-circle color-sombra-avatar"
+                src={`https://bootdey.com/img/Content/avatar/avatar${user.avatar}.png`}
+                title=""
+                alt=""
+              />
+            </div>
+          </div>
           <div className="col-lg-6">
             <div className="about-text go-to">
               <h3 className="dark-color">About Me</h3>
@@ -149,12 +226,21 @@ export default function CardEditarUsuario({ user, setUser }) {
                     )}
                     <div className="d-flex align-text-center gap-3">
                       {inputCiudad ? (
-                        <input
+                        <select
                           name="ciudad"
                           onChange={handleInput}
                           className="form-control"
-                          placeholder={`${user.ciudad}`}
-                        ></input>
+                        >
+                          <option value="">Selecciona una ciudad</option>
+                          <option value="Almería">Almería</option>
+                          <option value="Cádiz">Cádiz</option>
+                          <option value="Cordoba">Cordaba</option>
+                          <option value="Granada">Granada</option>
+                          <option value="Huelva">Huelva</option>
+                          <option value="Jaen">Jaen</option>
+                          <option value="Málaga">Málaga</option>
+                          <option value="Sevilla">Sevilla</option>
+                        </select>
                       ) : (
                         <p>{user.ciudad}</p>
                       )}
@@ -231,19 +317,19 @@ export default function CardEditarUsuario({ user, setUser }) {
                     </div>
                   </div>
                   <div className="media">
-                    <label>Fecha de alta</label>
+                    <label>Avatar</label>
                     <button
                       className="btn "
-                      onClick={() => setInputFecha(!inputFecha)}
+                      onClick={() => setInputAvatar(!inputAvatar)}
                     >
-                      {!inputFecha ? (
+                      {!inputAvatar ? (
                         <i className="bi bi-pencil text-primary"></i>
                       ) : (
                         <i className="bi bi-x-circle text-danger"></i>
                       )}
                     </button>
 
-                    {inputFecha ? (
+                    {inputAvatar ? (
                       <button className="btn" onClick={() => onSubmit()}>
                         {" "}
                         <i className="bi bi-check-circle text-success"></i>
@@ -251,30 +337,93 @@ export default function CardEditarUsuario({ user, setUser }) {
                     ) : (
                       ""
                     )}
-                    <div className="d-flex align-text-center gap-3">
-                      {inputFecha ? (
-                        <input
-                          name="tsAlta"
-                          onChange={handleInput}
-                          className="form-control"
-                          placeholder={`${user.tsAlta}`}
-                        ></input>
+                    <div className="d-flex ms-2 gap-3">
+                      {inputAvatar ? (
+                        <div className="d-flex flex-row ">
+                          <div className="col-4">
+                            <button
+                              className={`btn ${avatar2}`}
+                              onClick={() => matchAvatar(2)}
+                            >
+                              <img
+                                className="rounded-circle avatar"
+                                width="50"
+                                height="50"
+                                src="https://bootdey.com/img/Content/avatar/avatar2.png"
+                              ></img>
+                            </button>
+                            <button
+                              className={`btn ${avatar3}`}
+                              onClick={() => matchAvatar(3)}
+                            >
+                              <img
+                                className="rounded-circle"
+                                width="50"
+                                height="50"
+                                src="https://bootdey.com/img/Content/avatar/avatar3.png"
+                              ></img>
+                            </button>
+                            <button
+                              className={`btn ${avatar4}`}
+                              onClick={() => matchAvatar(4)}
+                            >
+                              <img
+                                className="rounded-circle"
+                                width="50"
+                                height="50"
+                                src="https://bootdey.com/img/Content/avatar/avatar4.png"
+                              ></img>
+                            </button>
+                          </div>
+                          <div className="col-4">
+                            <button
+                              className={`btn ${avatar5}`}
+                              onClick={() => matchAvatar(5)}
+                            >
+                              <img
+                                className="rounded-circle"
+                                width="50"
+                                height="50"
+                                src="https://bootdey.com/img/Content/avatar/avatar5.png"
+                              ></img>
+                            </button>
+                            <button
+                              className={`btn ${avatar8}`}
+                              onClick={() => matchAvatar(8)}
+                            >
+                              <img
+                                className="rounded-circle"
+                                width="50"
+                                height="50"
+                                src="https://bootdey.com/img/Content/avatar/avatar8.png"
+                              ></img>
+                            </button>
+                            <button
+                              className={`btn ${avatar7}`}
+                              onClick={() => matchAvatar(7)}
+                            >
+                              <img
+                                className="rounded-circle"
+                                width="50"
+                                height="50"
+                                src="https://bootdey.com/img/Content/avatar/avatar7.png"
+                              ></img>
+                            </button>
+                          </div>
+                        </div>
                       ) : (
-                        <p>{user.tsAlta}</p>
+                        <img
+                          className="rounded-circle"
+                          width="50"
+                          height="50"
+                          src={`https://bootdey.com/img/Content/avatar/avatar${user.avatar}.png`}
+                          size="rem"
+                        />
                       )}
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="col-lg-6">
-            <div className="about-avatar">
-              <img
-                src={`https://bootdey.com/img/Content/avatar/avatar${user.avatar}.png`}
-                title=""
-                alt=""
-              />
             </div>
           </div>
         </div>
