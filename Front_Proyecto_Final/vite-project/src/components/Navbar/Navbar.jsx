de37581fb511df2c1c5f909ee82b048f6413d46a
Franm0rales <picassomorales@gmail.com>
2023-02-13 17:26:20 +0100

    front

--- Front_Proyecto_Final/vite-project/src/components/Navbar/Navbar.jsx
+++ Front_Proyecto_Final/vite-project/src/components/Navbar/Navbar.jsx
@@ -6,273 +6,276 @@ export default function Navbar() {
   const { logout, authorization } = useAuthContext();
   return (
     <>
-    {authorization?.role==2 ?(
-      <>
-      <NavbarSuperior />
-      <header id="header" className="d-flex align-items-center sticky-top">
-        <div className="container d-flex align-items-center">
-          <div className="logo me-auto mt-3">
-            <h1>
-              <a className="">horizons</a>
-            </h1>
+      {authorization?.role == 2 ? (
+        <>
+          <NavbarSuperior />
+          <header id="header" className="d-flex align-items-center sticky-top">
+            <div className="container d-flex align-items-center">
+              <div className="logo me-auto mt-3">
+                <h1>
+                  <a className="">horizons</a>
+                </h1>
 
-            <a href="index.html">
-              <img src="assets/img/logo.png" alt="" className="img-fluid" />
-            </a>
-          </div>
-
-          <nav id="navbar" className="navbar">
-            <ul>
-              <li>
-                <Link to="/" className="nav-link scrollto active text-dark">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <a href="#" className="nav-link scrollto text-dark">
-                  Nosotros
+                <a href="index.html">
+                  <img src="assets/img/logo.png" alt="" className="img-fluid" />
                 </a>
-              </li>
+              </div>
 
-              <li>
-                <Link
-                  to="eventos"
-                  className="nav-link scrollto text-dark"
-                >
-                  Eventos
-                </Link>
-              </li>
-              <li className="dropdown">
-                <Link to="/empresas" className="text-decoration-none">
-                  <span>Empresas</span> <i className="bi bi-chevron-down"></i>
-                </Link>
+              <nav id="navbar" className="navbar">
                 <ul>
                   <li>
-                    <a href="#">Drop Down 1</a>
+                    <Link to="/" className="nav-link scrollto active text-dark">
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <a href="#" className="nav-link scrollto text-dark">
+                      Nosotros
+                    </a>
+                  </li>
+
+                  <li>
+                    <Link to="eventos" className="nav-link scrollto text-dark">
+                      Eventos
+                    </Link>
                   </li>
                   <li className="dropdown">
-                    <a href="#">
-                      <span>Deep Drop Down</span>{" "}
-                      <i className="bi bi-chevron-right"></i>
-                    </a>
+                    <Link to="/empresas" className="text-decoration-none">
+                      <span>Empresas</span>{" "}
+                      <i className="bi bi-chevron-down"></i>
+                    </Link>
                     <ul>
                       <li>
-                        <a href="#">Deep Drop Down 1</a>
+                        <a href="#">Drop Down 1</a>
+                      </li>
+                      <li className="dropdown">
+                        <a href="#">
+                          <span>Deep Drop Down</span>{" "}
+                          <i className="bi bi-chevron-right"></i>
+                        </a>
+                        <ul>
+                          <li>
+                            <a href="#">Deep Drop Down 1</a>
+                          </li>
+                          <li>
+                            <a href="#">Deep Drop Down 2</a>
+                          </li>
+                          <li>
+                            <a href="#">Deep Drop Down 3</a>
+                          </li>
+                          <li>
+                            <a href="#">Deep Drop Down 4</a>
+                          </li>
+                          <li>
+                            <a href="#">Deep Drop Down 5</a>
+                          </li>
+                        </ul>
                       </li>
                       <li>
-                        <a href="#">Deep Drop Down 2</a>
+                        <a href="#">Drop Down 2</a>
                       </li>
                       <li>
-                        <a href="#">Deep Drop Down 3</a>
+                        <a href="#">Drop Down 3</a>
                       </li>
                       <li>
-                        <a href="#">Deep Drop Down 4</a>
-                      </li>
-                      <li>
-                        <a href="#">Deep Drop Down 5</a>
+                        <a href="#">Drop Down 4</a>
                       </li>
                     </ul>
                   </li>
                   <li>
-                    <a href="#">Drop Down 2</a>
-                  </li>
-                  <li>
-                    <a href="#">Drop Down 3</a>
-                  </li>
-                  <li>
-                    <a href="#">Drop Down 4</a>
+                    <Link
+                      to="contacto"
+                      className="nav-link scrollto text-dark"
+                      href="#contact"
+                    >
+                      Contacto
+                    </Link>
                   </li>
                 </ul>
-              </li>
-              <li>
-                <Link
-                  to="contacto"
-                  className="nav-link scrollto text-dark"
-                  href="#contact"
-                >
-                  Contacto
-                </Link>
-              </li>
-            </ul>
-            <i className="bi bi-list mobile-nav-toggle"></i>
-            {!authorization?.email ? (
-              <Link to="login" className="nav-link scrollto text-dark">
-                Login
-              </Link>
-            ) : (
-              <Link
-                className="nav-link scrollto text-dark"
-                aria-current="page"
-                to="/"
-              >
-                <div className=" dropdown text-end mt-1 text-info fs-6 badge d-flex justify-content-center ">
-                  {" "}
-                  {authorization.email}
-                  <ul className="dropdown-menu text-small">
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        New project
-                      </a>
-                    </li>
-                    <li>
-                      <Link className="dropdown-item" to="editar">
-                        Profile
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="dropdown-item" to="test">
-                        Test vocacional
-                      </Link>
-                    </li>
-                    <li></li>
-                    <li>
-                      <Link onClick={logout} className="dropdown-item" to="/">
-                        Log Out
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </Link>
-            )}
-          </nav>
-        </div>
-      </header>
-      </>
+                <i className="bi bi-list mobile-nav-toggle"></i>
+                {!authorization?.email ? (
+                  <Link to="login" className="nav-link scrollto text-dark">
+                    Login
+                  </Link>
+                ) : (
+                  <Link
+                    className="nav-link scrollto text-dark"
+                    aria-current="page"
+                    to="/"
+                  >
+                    <div className=" dropdown text-end mt-1 text-info fs-6 badge d-flex justify-content-center ">
+                      {" "}
+                      {authorization.email}
+                      <ul className="dropdown-menu text-small">
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            New project
+                          </a>
+                        </li>
+                        <li>
+                          <Link className="dropdown-item" to="editar">
+                            Profile
+                          </Link>
+                        </li>
+                        <li>
+                          <Link className="dropdown-item" to="test">
+                            Test aptitudes
+                          </Link>
+                        </li>
+                        <li></li>
+                        <li>
+                          <Link
+                            onClick={logout}
+                            className="dropdown-item"
+                            to="/"
+                          >
+                            Log Out
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </Link>
+                )}
+              </nav>
+            </div>
+          </header>
+        </>
+      ) : (
+        <>
+          <NavbarSuperior />
+          <header id="header" className="d-flex align-items-center sticky-top">
+            <div className="container d-flex align-items-center">
+              <div className="logo me-auto mt-3">
+                <h1>
+                  <a className="">horizons</a>
+                </h1>
 
-    ): 
-    <>
-    <NavbarSuperior />
-    <header id="header" className="d-flex align-items-center sticky-top">
-      <div className="container d-flex align-items-center">
-        <div className="logo me-auto mt-3">
-          <h1>
-            <a className="">horizons</a>
-          </h1>
+                <a href="index.html">
+                  <img src="assets/img/logo.png" alt="" className="img-fluid" />
+                </a>
+              </div>
 
-          <a href="index.html">
-            <img src="assets/img/logo.png" alt="" className="img-fluid" />
-          </a>
-        </div>
-
-        <nav id="navbar" className="navbar">
-          <ul>
-            <li>
-              <Link to="/" className="nav-link scrollto active text-dark">
-                Home
-              </Link>
-            </li>
-            <li>
-              <a href="#" className="nav-link scrollto text-dark">
-                Nosotros
-              </a>
-            </li>
-
-            <li>
-              <Link
-                to="eventos"
-                className="nav-link scrollto text-dark"
-              >
-                Eventos
-              </Link>
-            </li>
-            <li className="dropdown">
-              <Link to="/empresas" className="text-decoration-none">
-                <span>Empresas</span> <i className="bi bi-chevron-down"></i>
-              </Link>
-              <ul>
-                <li>
-                  <a href="#">Drop Down 1</a>
-                </li>
-                <li className="dropdown">
-                  <a href="#">
-                    <span>Deep Drop Down</span>{" "}
-                    <i className="bi bi-chevron-right"></i>
-                  </a>
-                  <ul>
-                    <li>
-                      <a href="#">Deep Drop Down 1</a>
-                    </li>
-                    <li>
-                      <a href="#">Deep Drop Down 2</a>
-                    </li>
-                    <li>
-                      <a href="#">Deep Drop Down 3</a>
-                    </li>
-                    <li>
-                      <a href="#">Deep Drop Down 4</a>
-                    </li>
-                    <li>
-                      <a href="#">Deep Drop Down 5</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a href="#">Drop Down 2</a>
-                </li>
-                <li>
-                  <a href="#">Drop Down 3</a>
-                </li>
-                <li>
-                  <a href="#">Drop Down 4</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link
-                to="contacto"
-                className="nav-link scrollto text-dark"
-                href="#contact"
-              >
-                Contacto
-              </Link>
-            </li>
-          </ul>
-          <i className="bi bi-list mobile-nav-toggle"></i>
-          {!authorization?.email ? (
-            <Link to="login" className="nav-link scrollto text-dark">
-              Login
-            </Link>
-          ) : (
-            <Link
-              className="nav-link scrollto text-dark"
-              aria-current="page"
-              to="/"
-            >
-              <div className=" dropdown text-end mt-1 text-info fs-6 badge d-flex justify-content-center ">
-                {" "}
-                {authorization.email}
-                <ul className="dropdown-menu text-small">
+              <nav id="navbar" className="navbar">
+                <ul>
                   <li>
-                    <a className="dropdown-item" href="#">
-                      New project
+                    <Link to="/" className="nav-link scrollto active text-dark">
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <a href="#" className="nav-link scrollto text-dark">
+                      Nosotros
                     </a>
                   </li>
+
                   <li>
-                    <Link className="dropdown-item" to="editarempresa">
-                      Perfil Empresa
+                    <Link to="eventos" className="nav-link scrollto text-dark">
+                      Eventos
                     </Link>
                   </li>
-                  <li>
-                    <Link className="dropdown-item" to="agendaeventos">
-                      Agenda Eventos
+                  <li className="dropdown">
+                    <Link to="/empresas" className="text-decoration-none">
+                      <span>Empresas</span>{" "}
+                      <i className="bi bi-chevron-down"></i>
                     </Link>
+                    <ul>
+                      <li>
+                        <a href="#">Drop Down 1</a>
+                      </li>
+                      <li className="dropdown">
+                        <a href="#">
+                          <span>Deep Drop Down</span>{" "}
+                          <i className="bi bi-chevron-right"></i>
+                        </a>
+                        <ul>
+                          <li>
+                            <a href="#">Deep Drop Down 1</a>
+                          </li>
+                          <li>
+                            <a href="#">Deep Drop Down 2</a>
+                          </li>
+                          <li>
+                            <a href="#">Deep Drop Down 3</a>
+                          </li>
+                          <li>
+                            <a href="#">Deep Drop Down 4</a>
+                          </li>
+                          <li>
+                            <a href="#">Deep Drop Down 5</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="#">Drop Down 2</a>
+                      </li>
+                      <li>
+                        <a href="#">Drop Down 3</a>
+                      </li>
+                      <li>
+                        <a href="#">Drop Down 4</a>
+                      </li>
+                    </ul>
                   </li>
-                  <li></li>
                   <li>
-                    <Link onClick={logout} className="dropdown-item" to="/">
-                      Log Out
+                    <Link
+                      to="contacto"
+                      className="nav-link scrollto text-dark"
+                      href="#contact"
+                    >
+                      Contacto
                     </Link>
                   </li>
                 </ul>
-              </div>
-            </Link>
-          )}
-        </nav>
-      </div>
-    </header>
-    </>
-    }
-      
+                <i className="bi bi-list mobile-nav-toggle"></i>
+                {!authorization?.email ? (
+                  <Link to="login" className="nav-link scrollto text-dark">
+                    Login
+                  </Link>
+                ) : (
+                  <Link
+                    className="nav-link scrollto text-dark"
+                    aria-current="page"
+                    to="/"
+                  >
+                    <div className=" dropdown text-end mt-1 text-info fs-6 badge d-flex justify-content-center ">
+                      {" "}
+                      {authorization.email}
+                      <ul className="dropdown-menu text-small">
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            New project
+                          </a>
+                        </li>
+                        <li>
+                          <Link className="dropdown-item" to="editarempresa">
+                            Perfil Empresa
+                          </Link>
+                        </li>
+                        <li>
+                          <Link className="dropdown-item" to="agendaeventos">
+                            Agenda Eventos
+                          </Link>
+                        </li>
+                        <li></li>
+                        <li>
+                          <Link
+                            onClick={logout}
+                            className="dropdown-item"
+                            to="/"
+                          >
+                            Log Out
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </Link>
+                )}
+              </nav>
+            </div>
+          </header>
+        </>
+      )}
+
       <header className="header">
         <div className="container h-100">
           <div className="row  justify-content-center justify-content-lg-between text-light p-4">
--- Front_Proyecto_Final/vite-project/src/views/TestUsuario/TestUsuario.jsx
+++ Front_Proyecto_Final/vite-project/src/views/TestUsuario/TestUsuario.jsx
@@ -15,8 +15,52 @@ export default function TestUsuario() {
   return (
     <>
       <h2 id="test" className="section-title mt-5">
-        Test vocacional
+        Test aptitudes
       </h2>
+      <table class="table container col-8">
+        <thead>
+          <tr>
+            <th scope="col">Respuestas</th>
+            <th scope="col">Significado</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">
+              3 <i class="bi bi-arrow-right"></i>
+            </th>
+            <td>"TE CONSIDERAS MUY COMPETENTE"</td>
+          </tr>
+          <tr>
+            <th scope="row">
+              2 <i class="bi bi-arrow-right"></i>
+            </th>
+            <td>"TE CONSIDERAS COMPETENTE"</td>
+          </tr>
+          <tr>
+            <th scope="row">
+              1 <i class="bi bi-arrow-right"></i>
+            </th>
+
+            <td>"TE CONSIDERAS MEDIANAMENTE COMPETENTE"</td>
+          </tr>
+          <tr>
+            <th scope="row">
+              0 <i class="bi bi-arrow-right"></i>
+            </th>
+
+            <td>"TE CONSIDERAS INCOMPETENTE"</td>
+          </tr>
+        </tbody>
+      </table>
+      {/* <ul className="container">
+        <li className="list-unstyled">3 "TE CONSIDERAS MUY COMPETENTE"</li>
+        <li className="list-unstyled">2 "TE CONSIDERAS COMPETENTE"</li>
+        <li className="list-unstyled">
+          1 "TE CONSIDERAS MEDIANAMENTE COMPETENTE"
+        </li>
+        <li className="list-unstyled">0 "TE CONSIDERAS INCOMPETENTE"</li>
+      </ul> */}
       {test ? (
         test.map((pregunta) => (
           <PreguntasTest key={pregunta.id} test={pregunta.pregunta} />
