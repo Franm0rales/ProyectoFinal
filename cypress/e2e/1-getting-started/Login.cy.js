f28578ff08e023554c1700a2b075405acf5fdb61
Franm0rales <PICASSOMORALES@GMAIL.COM>
2023-02-18 21:45:55 +0100

    login

--- cypress/e2e/1-getting-started/Login.cy.js
+++ cypress/e2e/1-getting-started/Login.cy.js
@@ -9,7 +9,7 @@ describe('example Login to app', () => {
   describe('Prueba de inicio de sesión automático', () => {
     it('Inicia sesión automáticamente con credenciales válidas', () => {
       cy.visit('http://127.0.0.1:5173/login').wait(3000);
-      cy.get('[data-cy=email]').type('fran2@gmail.com').wait(3000);
+      cy.get('[data-cy=email]').type('alex@mail.com').wait(3000);
       cy.get('[data-cy=password]').type('Aa12345').wait(3000);
      
       cy.get('[data-cy=form-check-input]')
