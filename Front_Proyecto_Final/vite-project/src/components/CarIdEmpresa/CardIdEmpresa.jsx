7124315b801ee86f5992ef7f7052356f972a0443
Franm0rales <PICASSOMORALES@GMAIL.COM>
2023-02-20 23:16:10 +0100

    css

--- Front_Proyecto_Final/vite-project/src/components/CarIdEmpresa/CardIdEmpresa.jsx
+++ Front_Proyecto_Final/vite-project/src/components/CarIdEmpresa/CardIdEmpresa.jsx
@@ -19,7 +19,29 @@ export default function CardIdEmpresa({ users }) {
 
   return (
     <>
-      <div className="container d-flex justify-content-center">
+     <div id="card">
+     <div class="left">
+         <img src={galeria1} alt=""/>
+     </div>
+     <div class="right">
+        <div className=" d-flex">
+              <div className="card-body">
+                <h5 className="text-start pt-2">{users.nombre}</h5>
+                <p className="text-start"><i class="bi bi-textarea-resize text-primary fs-5"></i> CIF: {users.CIF}</p>
+                <p className=""> <i class="bi bi-buildings text-primary fs-5"></i> Ciudad: {users.ciudad}</p>
+                <p className=""><i className="bi bi-geo-alt text-primary fs-5"/> Direccion: {users.direccion}</p>
+                <p className=""><i class="bi bi-envelope text-primary fs-5"></i>{" "} Email: {users.email}</p>
+                
+
+                <p className="card-text">
+                  <small className="text-start"><i class="bi bi-telephone text-primary fs-5"></i> {users.telefono}</small>
+                </p>
+              </div>
+     </div>
+     </div>
+ </div>
+    
+      {/* <div className="container d-flex justify-content-center">
         <div className=" mb-3 mt-5 ml-4 " style={{ width: "90%" }}>
           <div className="row no-gutters ">
             <div className="col-md-4">
@@ -65,6 +87,7 @@ export default function CardIdEmpresa({ users }) {
           No
         </button>
       </div>
+      */}
     </>
   );
 }
