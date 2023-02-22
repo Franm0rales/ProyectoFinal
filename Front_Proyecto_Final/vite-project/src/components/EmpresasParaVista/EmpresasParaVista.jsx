3537bed1f0931dc74ad39a99ee7a49728413e1fc
Franm0rales <picassomorales@gmail.com>
2023-02-22 20:07:21 +0100

    css

--- Front_Proyecto_Final/vite-project/src/components/EmpresasParaVista/EmpresasParaVista.jsx
+++ Front_Proyecto_Final/vite-project/src/components/EmpresasParaVista/EmpresasParaVista.jsx
@@ -1,33 +1,106 @@
 export default function EmpresasParaVista() {
   return (
-    <div className="container">
-      <div class="card event-card mb-4">
-        <div class="row no-gutters">
-          <div class="col-md-4">
-            <img src="evento.jpg" class="card-img" alt="Imagen del evento" />
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">Nombre del evento</h5>
-              <p class="card-text">Descripción breve del evento.</p>
-              <p class="card-text">
-                <small class="text-muted">Fecha del evento</small>
-              </p>
+    <div class="container">
+      <h1>Events</h1>
+      <div class="row">
+        <article class="card fl-left">
+          <section class="date">
+            <time datetime="23th feb">
+              <span>23</span>
+              <span>feb</span>
+            </time>
+          </section>
+          <section class="card-cont">
+            <small>dj khaled</small>
+            <h3>live in sydney</h3>
+            <div class="even-date">
+              <i class="fa fa-calendar"></i>
+              <time>
+                <span>wednesday 28 december 2014</span>
+                <span>08:55pm to 12:00 am</span>
+              </time>
             </div>
-          </div>
-        </div>
-        <div class="card-overlay">
-          <div class="card-overlay-text">
-            <h5 class="card-title">Nombre del evento</h5>
-            <p class="card-text">Descripción breve del evento.</p>
-            <p class="card-text">
-              <small class="text-muted">Fecha del evento</small>
-            </p>
-            <a id="botones" href="#" class="btn btn-primary">
-              Más información
-            </a>
-          </div>
-        </div>
+            <div class="even-info">
+              <i class="fa fa-map-marker"></i>
+              <p>nexen square for people australia, sydney</p>
+            </div>
+            <a href="#">tickets</a>
+          </section>
+        </article>
+        <article class="card fl-left">
+          <section class="date">
+            <time datetime="23th feb">
+              <span>23</span>
+              <span>feb</span>
+            </time>
+          </section>
+          <section class="card-cont">
+            <small>dj khaled</small>
+            <h3>corner obsest program</h3>
+            <div class="even-date">
+              <i class="fa fa-calendar"></i>
+              <time>
+                <span>wednesday 28 december 2014</span>
+                <span>08:55pm to 12:00 am</span>
+              </time>
+            </div>
+            <div class="even-info">
+              <i class="fa fa-map-marker"></i>
+              <p>nexen square for people australia, sydney</p>
+            </div>
+            <a href="#">tickets</a>
+          </section>
+        </article>
+      </div>
+      <div class="row">
+        <article class="card fl-left">
+          <section class="date">
+            <time datetime="23th feb">
+              <span>23</span>
+              <span>feb</span>
+            </time>
+          </section>
+          <section class="card-cont">
+            <small>dj khaled</small>
+            <h3>music kaboom festivel</h3>
+            <div class="even-date">
+              <i class="fa fa-calendar"></i>
+              <time>
+                <span>wednesday 28 december 2014</span>
+                <span>08:55pm to 12:00 am</span>
+              </time>
+            </div>
+            <div class="even-info">
+              <i class="fa fa-map-marker"></i>
+              <p>nexen square for people australia, sydney</p>
+            </div>
+            <a href="#">booked</a>
+          </section>
+        </article>
+        <article class="card fl-left">
+          <section class="date">
+            <time datetime="23th feb">
+              <span>23</span>
+              <span>feb</span>
+            </time>
+          </section>
+          <section class="card-cont">
+            <small>dj khaled</small>
+            <h3>hello dubai festivel</h3>
+            <div class="even-date">
+              <i class="fa fa-calendar"></i>
+              <time>
+                <span>wednesday 28 december 2014</span>
+                <span>08:55pm to 12:00 am</span>
+              </time>
+            </div>
+            <div class="even-info">
+              <i class="fa fa-map-marker"></i>
+              <p>nexen square for people australia, sydney</p>
+            </div>
+            <a href="#">cancel</a>
+          </section>
+        </article>
       </div>
     </div>
   );
