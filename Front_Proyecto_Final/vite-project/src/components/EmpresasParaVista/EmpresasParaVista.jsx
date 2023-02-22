4c8c2e149f08edc81c871ecf4ab887d1674df89e
Franm0rales <PICASSOMORALES@GMAIL.COM>
2023-02-22 21:42:18 +0100

    css

--- Front_Proyecto_Final/vite-project/src/components/EmpresasParaVista/EmpresasParaVista.jsx
+++ Front_Proyecto_Final/vite-project/src/components/EmpresasParaVista/EmpresasParaVista.jsx
@@ -1,232 +1,75 @@
 export default function EmpresasParaVista() {
   return (
-    <div class="container">
-      <div class="col-md-12">
-        <div class="tarjeta b-1 hover-shadow mb-20">
-          <div class="media tarjeta-body">
-            <div class="media-left pr-12">
-              <img
-                class="avatar avatar-xl no-radius"
-                src="https://bootdey.com/img/Content/avatar/avatar1.png"
-                alt="..."
-              />
+    <div class="container mt-5">
+
+    <div class="row align-items-center event-block no-gutters margin-40px-bottom">
+        <div class="col-lg-5 col-sm-12">
+            <div class="position-relative">
+                <img src="https://www.bootdey.com/image/450x280/FFB6C1/000000" alt=""/>
+                <div class="events-date">
+                    <div class="font-size28">10</div>
+                    <div class="font-size14">Mar</div>
+                </div>
             </div>
-            <div class="media-body">
-              <div class="mb-2">
-                <span class="fs-20 pr-16">Hossein Shams</span>
-              </div>
-              <small class="fs-16 fw-300 ls-1">Front-end developer</small>
-            </div>
-            <div class="media-right text-right d-none d-md-block">
-              <p class="fs-14 text-fade mb-12">
-                <i class="fa fa-map-marker pr-1"></i> Menlo Park, CA
-              </p>
-              <span class="text-fade">
-                <i class="fa fa-money pr-1"></i> $60 per hour
-              </span>
-            </div>
-          </div>
-          <footer class="tarjeta-footer flexbox align-items-center">
-            <div>
-              <strong>Applied on:</strong>
-              <span>21 Jan, 2017</span>
-            </div>
-            <div class="tarjeta-hover-show">
-              <a class="btn btn-xs fs-10 btn-bold btn-info" href="#">
-                Download CV
-              </a>
-              <a
-                class="btn btn-xs fs-10 btn-bold btn-primary"
-                href="#"
-                data-toggle="modal"
-                data-target="#modal-contact"
-              >
-                Contact
-              </a>
-              <a class="btn btn-xs fs-10 btn-bold btn-warning" href="#">
-                Delete
-              </a>
-            </div>
-          </footer>
         </div>
-
-        <div class="tarjeta b-1 hover-shadow mb-20">
-          <div class="media tarjeta-body">
-            <div class="media-left pr-12">
-              <img
-                class="avatar avatar-xl no-radius"
-                src="https://bootdey.com/img/Content/avatar/avatar2.png"
-                alt="..."
-              />
+        <div class="col-lg-7 col-sm-12">
+            <div class="padding-60px-lr md-padding-50px-lr sm-padding-30px-all xs-padding-25px-all">
+                <h5 class="margin-15px-bottom md-margin-10px-bottom font-size22 md-font-size20 xs-font-size18 font-weight-500"><a href="event-details.html" class="text-theme-color">Business Conference</a></h5>
+                <ul class="event-time margin-10px-bottom md-margin-5px-bottom">
+                    <li><i class="far fa-clock margin-10px-right"></i> 01:30 PM - 04:30 PM</li>
+                    <li><i class="fas fa-user margin-5px-right"></i> Speaker : John Sminth</li>
+                </ul>
+                <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore</p>
+                <a class="butn small margin-10px-top md-no-margin-top" href="event-details.html">Read More <i class="fas fa-long-arrow-alt-right margin-10px-left"></i></a>
             </div>
-            <div class="media-body">
-              <div class="mb-2">
-                <span class="fs-20 pr-16">Maryam Amiri</span>
-              </div>
-              <small class="fs-16 fw-300 ls-1">Designer</small>
-            </div>
-            <div class="media-right text-right d-none d-md-block">
-              <p class="fs-14 text-fade mb-12">
-                <i class="fa fa-map-marker pr-1"></i> Fairfield, IA
-              </p>
-              <span class="text-fade">
-                <i class="fa fa-money pr-1"></i> $45 per hour
-              </span>
-            </div>
-          </div>
-
-          <footer class="tarjeta-footer flexbox align-items-center">
-            <div>
-              <strong>Applied on:</strong>
-              <span>17 Jan, 2017</span>
-            </div>
-            <div class="tarjeta-hover-show">
-              <a class="btn btn-xs fs-10 btn-bold btn-info" href="#">
-                Download CV
-              </a>
-              <a
-                class="btn btn-xs fs-10 btn-bold btn-primary"
-                href="#"
-                data-toggle="modal"
-                data-target="#modal-contact"
-              >
-                Contact
-              </a>
-              <a class="btn btn-xs fs-10 btn-bold btn-warning" href="#">
-                Delete
-              </a>
-            </div>
-          </footer>
         </div>
-
-        <div class="tarjeta b-1 hover-shadow mb-20">
-          <div class="media tarjeta-body">
-            <div class="media-left pr-12">
-              <img
-                class="avatar avatar-xl no-radius"
-                src="https://bootdey.com/img/Content/avatar/avatar3.png"
-                alt="..."
-              />
-            </div>
-            <div class="media-body">
-              <div class="mb-2">
-                <span class="fs-20 pr-16">Frank Camly</span>
-              </div>
-              <small class="fs-16 fw-300 ls-1">Front end developer</small>
-            </div>
-            <div class="media-right text-right d-none d-md-block">
-              <p class="fs-14 text-fade mb-12">
-                <i class="fa fa-map-marker pr-1"></i> Seattle, WA
-              </p>
-              <span class="text-fade">
-                <i class="fa fa-money pr-1"></i> $40 per hour
-              </span>
-            </div>
-          </div>
-
-          <footer class="tarjeta-footer flexbox align-items-center">
-            <div>
-              <strong>Applied on:</strong>
-              <span>15 Jan, 2017</span>
-            </div>
-            <div class="tarjeta-hover-show">
-              <a class="btn btn-xs fs-10 btn-bold btn-info" href="#">
-                Download CV
-              </a>
-              <a
-                class="btn btn-xs fs-10 btn-bold btn-primary"
-                href="#"
-                data-toggle="modal"
-                data-target="#modal-contact"
-              >
-                Contact
-              </a>
-              <a class="btn btn-xs fs-10 btn-bold btn-warning" href="#">
-                Delete
-              </a>
-            </div>
-          </footer>
-        </div>
-
-        <div class="tarjeta b-1 hover-shadow mb-20">
-          <div class="media tarjeta-body">
-            <div class="media-left pr-12">
-              <img
-                class="avatar avatar-xl no-radius"
-                src="https://bootdey.com/img/Content/avatar/avatar4.png"
-                alt="..."
-              />
-            </div>
-            <div class="media-body">
-              <div class="mb-2">
-                <span class="fs-20 pr-16">Tim Hank</span>
-              </div>
-              <small class="fs-16 fw-300 ls-1">Web developer</small>
-            </div>
-            <div class="media-right text-right d-none d-md-block">
-              <p class="fs-14 text-fade mb-12">
-                <i class="fa fa-map-marker pr-1"></i> Fremont, CA
-              </p>
-              <span class="text-fade">
-                <i class="fa fa-money pr-1"></i> $55 per hour
-              </span>
-            </div>
-          </div>
-
-          <footer class="tarjeta-footer flexbox align-items-center">
-            <div>
-              <strong>Applied on:</strong>
-              <span>11 Jan, 2017</span>
-            </div>
-            <div class="tarjeta-hover-show">
-              <a class="btn btn-xs fs-10 btn-bold btn-info" href="#">
-                Download CV
-              </a>
-              <a
-                class="btn btn-xs fs-10 btn-bold btn-primary"
-                href="#"
-                data-toggle="modal"
-                data-target="#modal-contact"
-              >
-                Contact
-              </a>
-              <a class="btn btn-xs fs-10 btn-bold btn-warning" href="#">
-                Delete
-              </a>
-            </div>
-          </footer>
-        </div>
-
-        <nav>
-          <ul class="pagination justify-content-center">
-            <li class="page-item active">
-              <a class="page-link" href="#">
-                1
-              </a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="#">
-                2
-              </a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="#">
-                3
-              </a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="#">
-                4
-              </a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" href="#">
-                5
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
     </div>
+
+    <div class="row align-items-center event-block no-gutters margin-40px-bottom">
+        <div class="col-lg-7 order-2 order-lg-1">
+            <div class="padding-60px-lr md-padding-50px-lr sm-padding-30px-all xs-padding-25px-all">
+                <h5 class="margin-15px-bottom md-margin-10px-bottom font-size22 md-font-size20 xs-font-size18 font-weight-500"><a href="event-details.html" class="text-theme-color">Craft Workshops</a></h5>
+                <ul class="event-time margin-10px-bottom md-margin-5px-bottom">
+                    <li><i class="far fa-clock margin-10px-right"></i> 09:00 AM - 09:30 PM</li>
+                    <li><i class="fas fa-user margin-5px-right"></i> Speaker : Alexa Zone</li>
+                </ul>
+                <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore</p>
+                <a class="butn small margin-10px-top md-no-margin-top" href="event-details.html">Read More <i class="fas fa-long-arrow-alt-right margin-10px-left"></i></a>
+            </div>
+        </div>
+        <div class="col-lg-5 order-1 order-lg-2">
+            <div class="position-relative">
+                <img src="https://www.bootdey.com/image/450x280/87CEFA/000000" alt=""/>
+                <div class="events-date">
+                    <div class="font-size28">25</div>
+                    <div class="font-size14">Feb</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="row align-items-center event-block no-gutters margin-40px-bottom">
+        <div class="col-lg-5 col-sm-12">
+            <div class="position-relative">
+                <img src="https://www.bootdey.com/image/450x280/B0C4DE/000000" alt=""/>
+                <div class="events-date">
+                    <div class="font-size28">20</div>
+                    <div class="font-size14">Feb</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-7 col-sm-12">
+            <div class="padding-60px-lr md-padding-50px-lr sm-padding-30px-all xs-padding-25px-all">
+                <h5 class="margin-15px-bottom md-margin-10px-bottom font-size22 md-font-size20 xs-font-size18 font-weight-500"><a href="event-details.html" class="text-theme-color">Mentorship Program</a></h5>
+                <ul class="event-time margin-10px-bottom md-margin-5px-bottom">
+                    <li><i class="far fa-clock margin-10px-right"></i> 09:20 AM - 12:00 AM</li>
+                    <li><i class="fas fa-user margin-5px-right"></i> Speaker : Avil Mex</li>
+                </ul>
+                <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore</p>
+                <a class="butn small margin-10px-top md-no-margin-top" href="event-details.html">Read More <i class="fas fa-long-arrow-alt-right margin-10px-left"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
   );
 }
