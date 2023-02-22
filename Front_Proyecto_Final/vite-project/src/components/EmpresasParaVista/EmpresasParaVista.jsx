a529d89607a5ef8f42e00bde41203a3f3dbd0864
Franm0rales <picassomorales@gmail.com>
2023-02-22 20:23:59 +0100

    css

--- Front_Proyecto_Final/vite-project/src/components/EmpresasParaVista/EmpresasParaVista.jsx
+++ Front_Proyecto_Final/vite-project/src/components/EmpresasParaVista/EmpresasParaVista.jsx
@@ -1,106 +1,231 @@
 export default function EmpresasParaVista() {
   return (
     <div class="container">
-      <h1>Events</h1>
-      <div class="row">
-        <article class="card fl-left">
-          <section class="date">
-            <time datetime="23th feb">
-              <span>23</span>
-              <span>feb</span>
-            </time>
-          </section>
-          <section class="card-cont">
-            <small>dj khaled</small>
-            <h3>live in sydney</h3>
-            <div class="even-date">
-              <i class="fa fa-calendar"></i>
-              <time>
-                <span>wednesday 28 december 2014</span>
-                <span>08:55pm to 12:00 am</span>
-              </time>
+      <div class="col-md-12">
+        <div class="tarjeta b-1 hover-shadow mb-20">
+          <div class="media tarjeta-body">
+            <div class="media-left pr-12">
+              <img
+                class="avatar avatar-xl no-radius"
+                src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                alt="..."
+              />
             </div>
-            <div class="even-info">
-              <i class="fa fa-map-marker"></i>
-              <p>nexen square for people australia, sydney</p>
+            <div class="media-body">
+              <div class="mb-2">
+                <span class="fs-20 pr-16">Hossein Shams</span>
+              </div>
+              <small class="fs-16 fw-300 ls-1">Front-end developer</small>
             </div>
-            <a href="#">tickets</a>
-          </section>
-        </article>
-        <article class="card fl-left">
-          <section class="date">
-            <time datetime="23th feb">
-              <span>23</span>
-              <span>feb</span>
-            </time>
-          </section>
-          <section class="card-cont">
-            <small>dj khaled</small>
-            <h3>corner obsest program</h3>
-            <div class="even-date">
-              <i class="fa fa-calendar"></i>
-              <time>
-                <span>wednesday 28 december 2014</span>
-                <span>08:55pm to 12:00 am</span>
-              </time>
+            <div class="media-right text-right d-none d-md-block">
+              <p class="fs-14 text-fade mb-12">
+                <i class="fa fa-map-marker pr-1"></i> Menlo Park, CA
+              </p>
+              <span class="text-fade">
+                <i class="fa fa-money pr-1"></i> $60 per hour
+              </span>
             </div>
-            <div class="even-info">
-              <i class="fa fa-map-marker"></i>
-              <p>nexen square for people australia, sydney</p>
+          </div>
+          <footer class="tarjeta-footer flexbox align-items-center">
+            <div>
+              <strong>Applied on:</strong>
+              <span>21 Jan, 2017</span>
             </div>
-            <a href="#">tickets</a>
-          </section>
-        </article>
-      </div>
-      <div class="row">
-        <article class="card fl-left">
-          <section class="date">
-            <time datetime="23th feb">
-              <span>23</span>
-              <span>feb</span>
-            </time>
-          </section>
-          <section class="card-cont">
-            <small>dj khaled</small>
-            <h3>music kaboom festivel</h3>
-            <div class="even-date">
-              <i class="fa fa-calendar"></i>
-              <time>
-                <span>wednesday 28 december 2014</span>
-                <span>08:55pm to 12:00 am</span>
-              </time>
+            <div class="tarjeta-hover-show">
+              <a class="btn btn-xs fs-10 btn-bold btn-info" href="#">
+                Download CV
+              </a>
+              <a
+                class="btn btn-xs fs-10 btn-bold btn-primary"
+                href="#"
+                data-toggle="modal"
+                data-target="#modal-contact"
+              >
+                Contact
+              </a>
+              <a class="btn btn-xs fs-10 btn-bold btn-warning" href="#">
+                Delete
+              </a>
             </div>
-            <div class="even-info">
-              <i class="fa fa-map-marker"></i>
-              <p>nexen square for people australia, sydney</p>
+          </footer>
+        </div>
+
+        <div class="tarjeta b-1 hover-shadow mb-20">
+          <div class="media tarjeta-body">
+            <div class="media-left pr-12">
+              <img
+                class="avatar avatar-xl no-radius"
+                src="https://bootdey.com/img/Content/avatar/avatar2.png"
+                alt="..."
+              />
             </div>
-            <a href="#">booked</a>
-          </section>
-        </article>
-        <article class="card fl-left">
-          <section class="date">
-            <time datetime="23th feb">
-              <span>23</span>
-              <span>feb</span>
-            </time>
-          </section>
-          <section class="card-cont">
-            <small>dj khaled</small>
-            <h3>hello dubai festivel</h3>
-            <div class="even-date">
-              <i class="fa fa-calendar"></i>
-              <time>
-                <span>wednesday 28 december 2014</span>
-                <span>08:55pm to 12:00 am</span>
-              </time>
+            <div class="media-body">
+              <div class="mb-2">
+                <span class="fs-20 pr-16">Maryam Amiri</span>
+              </div>
+              <small class="fs-16 fw-300 ls-1">Designer</small>
             </div>
-            <div class="even-info">
-              <i class="fa fa-map-marker"></i>
-              <p>nexen square for people australia, sydney</p>
+            <div class="media-right text-right d-none d-md-block">
+              <p class="fs-14 text-fade mb-12">
+                <i class="fa fa-map-marker pr-1"></i> Fairfield, IA
+              </p>
+              <span class="text-fade">
+                <i class="fa fa-money pr-1"></i> $45 per hour
+              </span>
             </div>
-            <a href="#">cancel</a>
-          </section>
-        </article>
+          </div>
+
+          <footer class="tarjeta-footer flexbox align-items-center">
+            <div>
+              <strong>Applied on:</strong>
+              <span>17 Jan, 2017</span>
+            </div>
+            <div class="tarjeta-hover-show">
+              <a class="btn btn-xs fs-10 btn-bold btn-info" href="#">
+                Download CV
+              </a>
+              <a
+                class="btn btn-xs fs-10 btn-bold btn-primary"
+                href="#"
+                data-toggle="modal"
+                data-target="#modal-contact"
+              >
+                Contact
+              </a>
+              <a class="btn btn-xs fs-10 btn-bold btn-warning" href="#">
+                Delete
+              </a>
+            </div>
+          </footer>
+        </div>
+
+        <div class="tarjeta b-1 hover-shadow mb-20">
+          <div class="media tarjeta-body">
+            <div class="media-left pr-12">
+              <img
+                class="avatar avatar-xl no-radius"
+                src="https://bootdey.com/img/Content/avatar/avatar3.png"
+                alt="..."
+              />
+            </div>
+            <div class="media-body">
+              <div class="mb-2">
+                <span class="fs-20 pr-16">Frank Camly</span>
+              </div>
+              <small class="fs-16 fw-300 ls-1">Front end developer</small>
+            </div>
+            <div class="media-right text-right d-none d-md-block">
+              <p class="fs-14 text-fade mb-12">
+                <i class="fa fa-map-marker pr-1"></i> Seattle, WA
+              </p>
+              <span class="text-fade">
+                <i class="fa fa-money pr-1"></i> $40 per hour
+              </span>
+            </div>
+          </div>
+
+          <footer class="tarjeta-footer flexbox align-items-center">
+            <div>
+              <strong>Applied on:</strong>
+              <span>15 Jan, 2017</span>
+            </div>
+            <div class="tarjeta-hover-show">
+              <a class="btn btn-xs fs-10 btn-bold btn-info" href="#">
+                Download CV
+              </a>
+              <a
+                class="btn btn-xs fs-10 btn-bold btn-primary"
+                href="#"
+                data-toggle="modal"
+                data-target="#modal-contact"
+              >
+                Contact
+              </a>
+              <a class="btn btn-xs fs-10 btn-bold btn-warning" href="#">
+                Delete
+              </a>
+            </div>
+          </footer>
+        </div>
+
+        <div class="tarjeta b-1 hover-shadow mb-20">
+          <div class="media tarjeta-body">
+            <div class="media-left pr-12">
+              <img
+                class="avatar avatar-xl no-radius"
+                src="https://bootdey.com/img/Content/avatar/avatar4.png"
+                alt="..."
+              />
+            </div>
+            <div class="media-body">
+              <div class="mb-2">
+                <span class="fs-20 pr-16">Tim Hank</span>
+              </div>
+              <small class="fs-16 fw-300 ls-1">Web developer</small>
+            </div>
+            <div class="media-right text-right d-none d-md-block">
+              <p class="fs-14 text-fade mb-12">
+                <i class="fa fa-map-marker pr-1"></i> Fremont, CA
+              </p>
+              <span class="text-fade">
+                <i class="fa fa-money pr-1"></i> $55 per hour
+              </span>
+            </div>
+          </div>
+
+          <footer class="tarjeta-footer flexbox align-items-center">
+            <div>
+              <strong>Applied on:</strong>
+              <span>11 Jan, 2017</span>
+            </div>
+            <div class="tarjeta-hover-show">
+              <a class="btn btn-xs fs-10 btn-bold btn-info" href="#">
+                Download CV
+              </a>
+              <a
+                class="btn btn-xs fs-10 btn-bold btn-primary"
+                href="#"
+                data-toggle="modal"
+                data-target="#modal-contact"
+              >
+                Contact
+              </a>
+              <a class="btn btn-xs fs-10 btn-bold btn-warning" href="#">
+                Delete
+              </a>
+            </div>
+          </footer>
+        </div>
+
+        <nav>
+          <ul class="pagination justify-content-center">
+            <li class="page-item active">
+              <a class="page-link" href="#">
+                1
+              </a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">
+                2
+              </a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">
+                3
+              </a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">
+                4
+              </a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="#">
+                5
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
