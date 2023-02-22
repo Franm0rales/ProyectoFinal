df1ee7daec91c48da1ed19a181949cf236749474
Franm0rales <picassomorales@gmail.com>
2023-02-22 10:14:12 +0100

    dropdown

--- Front_Proyecto_Final/vite-project/src/components/Navbar/Navbar.jsx
+++ Front_Proyecto_Final/vite-project/src/components/Navbar/Navbar.jsx
@@ -42,45 +42,7 @@ export default function Navbar() {
                   <li className="dropdown">
                     <Link to="/empresas" className="text-decoration-none">
                       <span>Empresas</span>{" "}
-                      <i className="bi bi-chevron-down"></i>
                     </Link>
-                    <ul>
-                      <li>
-                        <a href="#">Drop Down 1</a>
-                      </li>
-                      <li className="dropdown">
-                        <a href="#">
-                          <span>Deep Drop Down</span>{" "}
-                          <i className="bi bi-chevron-right"></i>
-                        </a>
-                        <ul>
-                          <li>
-                            <a href="#">Deep Drop Down 1</a>
-                          </li>
-                          <li>
-                            <a href="#">Deep Drop Down 2</a>
-                          </li>
-                          <li>
-                            <a href="#">Deep Drop Down 3</a>
-                          </li>
-                          <li>
-                            <a href="#">Deep Drop Down 4</a>
-                          </li>
-                          <li>
-                            <a href="#">Deep Drop Down 5</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li>
-                        <a href="#">Drop Down 2</a>
-                      </li>
-                      <li>
-                        <a href="#">Drop Down 3</a>
-                      </li>
-                      <li>
-                        <a href="#">Drop Down 4</a>
-                      </li>
-                    </ul>
                   </li>
                 </ul>
                 <i className="bi bi-list mobile-nav-toggle"></i>
@@ -165,45 +127,7 @@ export default function Navbar() {
                   <li className="dropdown">
                     <Link to="/empresas" className="text-decoration-none">
                       <span>Empresas</span>{" "}
-                      <i className="bi bi-chevron-down"></i>
                     </Link>
-                    <ul>
-                      <li>
-                        <a href="#">Drop Down 1</a>
-                      </li>
-                      <li className="dropdown">
-                        <a href="#">
-                          <span>Deep Drop Down</span>{" "}
-                          <i className="bi bi-chevron-right"></i>
-                        </a>
-                        <ul>
-                          <li>
-                            <a href="#">Deep Drop Down 1</a>
-                          </li>
-                          <li>
-                            <a href="#">Deep Drop Down 2</a>
-                          </li>
-                          <li>
-                            <a href="#">Deep Drop Down 3</a>
-                          </li>
-                          <li>
-                            <a href="#">Deep Drop Down 4</a>
-                          </li>
-                          <li>
-                            <a href="#">Deep Drop Down 5</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li>
-                        <a href="#">Drop Down 2</a>
-                      </li>
-                      <li>
-                        <a href="#">Drop Down 3</a>
-                      </li>
-                      <li>
-                        <a href="#">Drop Down 4</a>
-                      </li>
-                    </ul>
                   </li>
                 </ul>
                 <i className="bi bi-list mobile-nav-toggle"></i>
