9551533720ddc3f9e57f290bae510b4949af0071
Franm0rales <PICASSOMORALES@GMAIL.COM>
2023-02-25 12:08:13 +0100

    css

--- Front_Proyecto_Final/vite-project/src/components/Descripcion/Descripcion.jsx
+++ Front_Proyecto_Final/vite-project/src/components/Descripcion/Descripcion.jsx
@@ -1,12 +1,15 @@
 import imgvideo from "../../assets/releevant.jpeg";
 export default function Descripcion() {
   return (
+  <>
+    <h1 className="text-center pt-5 texto-contacto">¿Qué es Horizons?</h1>
     <section id="about" className="about">
       <div className="container" data-aos="fade-up">
         <div className="row no-gutters rounded">
           <div className="col-lg-6 ">
-            <img src={imgvideo} className="img-fluid pt-5 pb-5 img-releevant" alt="" />
-            
+            <div className="imagen">
+            <img src={imgvideo} className="img-fluid pt-3 pb-5 img-releevant" alt="" />
+            </div>
             <div className="col-12 d-flex justify-content-between align-items-center">
             <div className="icon-box pb-5 pt-2" data-aos="fade-up" data-aos-delay="100">
               <div className="icon">
@@ -33,16 +36,16 @@ export default function Descripcion() {
           </div>
 
           <div className="col-lg-6 d-flex  about-content">
-            <div className="section-title">
-              <h1>¿Qué es Horizons?</h1>
-              <p className="mt-1 fs-5">
+            <div className="section-title ">
+             
+              <p className="mt-2 fs-5">
                Nuestra página web ofrece a los estudiantes oportunidades para conocer y experimentar diferentes puestos de trabajo a través de días de voluntariado en empresas. Proporcionamos información detallada sobre las empresas que ofrecen estas oportunidades, los proyectos en los que se puede participar y los beneficios que se obtienen. Creemos que esta experiencia puede ser valiosa para los estudiantes para explorar sus intereses, descubrir nuevas habilidades y conocer el mundo laboral desde una perspectiva práctica. También ofrecemos un feedback con las empresas para mejorar los programas de voluntariado y ayudar a los estudiantes a encontrar oportunidades de trabajo y desarrollar habilidades valiosas.
               </p>
               <hr></hr>
               <div>
                
               </div>
-              <table className="mt-5" style={{borderCollapse: 'collapse'}}>
+              <table className="mt-2" style={{borderCollapse: 'collapse'}}>
   <thead>
     <tr>
       <th style={{borderRight: '2px solid #d9e8f4', padding: '8px'}}>Empresas que ofrecen días de voluntariado</th>
@@ -63,5 +66,6 @@ export default function Descripcion() {
         </div>
       </div>
     </section>
+    </>
   );
 }
