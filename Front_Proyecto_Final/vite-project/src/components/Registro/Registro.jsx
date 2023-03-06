d1d47807b9cd591ea5e9cb712238916861f6373b
AlexGomezR <17gomezreyes@gmail.com>
2023-03-06 19:03:08 +0100

    crear perfiles empresa

--- Front_Proyecto_Final/vite-project/src/components/Registro/Registro.jsx
+++ Front_Proyecto_Final/vite-project/src/components/Registro/Registro.jsx
@@ -187,7 +187,7 @@ export default function Registro() {
             <option value="">Selecciona una ciudad</option>
             <option value="Almeria">Almeria</option>
             <option value="Cadiz">Cadiz</option>
-            <option value="Cordoba">Córdpba</option>
+            <option value="Cordoba">Córdoba</option>
             <option value="Granada">Granada</option>
             <option value="Huelva">Huelva</option>
             <option value="Jaen">Jaen</option>
